8039ca2231560fff3f97868c96f9ecfdbddd5ddb
Bangmek1999 <54341777+Bangmek1999@users.noreply.github.com>
2021-10-11 12:21:44 +0700

    Update Liff_Profile.js

--- component/Liff_Profile.js
+++ component/Liff_Profile.js
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 
 export default function Liff_Profile() {
   const [profile, setProfile] = useState({});
+  const [email, setEmail] = useState({});
 
   useEffect(async () => {
     const liff = (await import("@line/liff")).default;
     await liff.ready;
     const profile = await liff.getProfile();
+    const email = liff.getDecodedIDToken().email;
+    setEmail(email);
     setProfile(profile);
   }, [profile.userId]);
   return (
@@ -26,6 +29,7 @@ export default function Liff_Profile() {
         <div> Name: {profile.displayName} </div>
         <div> Status: {profile.statusMessage} </div>
         <div> id: {profile.userId} </div>
+        <div> Email: {email} </div>
 
       </div>
     </div>
